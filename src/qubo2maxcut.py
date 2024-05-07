41841644e9bc2b5661593d337acd8272082083cb
Roger Serrat i Guevara <rogerserrat11@gmail.com>
2024-05-07 16:40:19 +0000

    Qubo 2 maxcut: add networkx check

--- src/qubo2maxcut.py
+++ src/qubo2maxcut.py
@@ -27,7 +27,7 @@ def qubo2maxcut(dists):
     # QUBO parameters 
     # A > B * N * max(distance)
     B = 1
-    A = (B * N * max_dist)
+    A = 2*(B * N * max_dist)
 
     # QUBO coefficients
     q = np.zeros((N*N, N*N))
@@ -42,39 +42,45 @@ def qubo2maxcut(dists):
             q[x,y] += A
         if k+1 == l:
             q[x,y] += B*dists[i,j]
-    print(q)
     
     # MaxCut weights
     graph = nx.Graph()
     for j in range(N*N):
-        # Remove diagonal from the sum!
         a = np.sum(q[:,j])
         b = np.sum(q[j,:])
         graph.add_edge(0, j+1, weight=a+b)
     for i in range(N*N):
         for j in range(0, i):
-            graph.add_edge(i+1, j+1, weight=q[i,j]+q[j,i])
+            w = q[i,j] + q[j,i]
+            if w != 0:
+                # Ignore edges with zero weight
+                graph.add_edge(i+1, j+1, weight=w)
     return graph
 
 
 if __name__ == "__main__":
-    N = 4
-    nodes = np.random.rand(N, 2) * 10  # Random nodes
-    matrix = np.zeros((N, N))
-    for i in range(N):
-        for j in range(N):
-            matrix[i, j] = np.linalg.norm(nodes[i] - nodes[j])
 
+    # Example: 4 nodes
     matrix = np.zeros((4,4))
-    matrix += 0.1
     matrix[0,1] = matrix[1,0] = 1
     matrix[0,2] = matrix[2,0] = 1
+    matrix[0,3] = matrix[3,0] = 2
+    matrix[1,2] = matrix[2,1] = 2
     matrix[1,3] = matrix[3,1] = 1
     matrix[2,3] = matrix[3,2] = 1
 
-    matrix = np.ones((2,2))
+    # Example: 2 nodes (trivial solution, used to check constraints)
+    #matrix = np.ones((2,2))
 
+    # Transform TSP graph to MaxCut graph
     graph = qubo2maxcut(matrix)
     #print(graph.edges(data="weight"))
-    res = qaoa_maxcut.run_qaoa_maxcut(graph, 8)
+    
+    # Run MaxCut on networkX (approximated)
+    val, part = nx.approximation.one_exchange(graph, weight="weight")
+    print(val)
+    print(part)
+
+    # Run MaxCut with QAOA
+    res = qaoa_maxcut.run_qaoa_maxcut(graph, 4)
     print(res)
